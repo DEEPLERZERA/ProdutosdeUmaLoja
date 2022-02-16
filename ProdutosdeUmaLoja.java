cf67511643fa2b7110e6f9648081af6963fca2d6
Daniel Borges Valentim <73613620+DEEPLERZERA@users.noreply.github.com>
2022-02-16 16:12:21 -0300

    indentação
    indentação

--- ProdutosdeUmaLoja.java
+++ ProdutosdeUmaLoja.java
@@ -5,10 +5,10 @@ import java.io.*; //Importando bibliotecas
 class ProdutosdeUmaLoja   //Criando classe do programa
 {
 
-   public static void main(String args[]) throws IOException  //Inicializando método main
+   public static void main(String args[]) throws IOException  //Inicializando mÃ©todo main
    {
 
-      String[] IngredienteNome = { "CafeEmPo", "Cookie", "Leite", "Pao", "cha", "frios", "Chocolate"};           //Criação de Arrays para armazenar todos os dados no banco
+      String[] IngredienteNome = { "CafeEmPo", "Cookie", "Leite", "Pao", "cha", "frios", "Chocolate"};           //CriaÃ§Ã£o de Arrays para armazenar todos os dados no banco
       String[] IngredienteUnidadedeMedida = {"ML", "G", "L", "G", "ML", "G"};
       String[] IngredientePrecoUnidade = {"5Reais", "3Reais", "5Reais", "1Reais", "2.50Reais", "3Reais"};
       String[] ProdutoNome = {"CafecomLeite", "CookieRecheado", "PaocomMortandela", "Cha"};
@@ -16,31 +16,31 @@ class ProdutosdeUmaLoja   //Criando classe do programa
       String[] ProdutoIngredientes = {"CafeeLeite", "CookieChocolate","PaoeFrios","Cha"};
       int[] IngredienteQuantidade = {5,8,2,15,2,27,52};
 
-      System.out.println("Produtos: ");            //Imprimindo na tela as opções disponíveis
+          System.out.println("Produtos: ");            //Imprimindo na tela as opÃ§Ãµes disponÃ­veis
 	  System.out.println("\n1-CafecomLeite");
 	  System.out.println("\n2-CookieRecheado");
 	  System.out.println("\n3-PaocomMortandela");
 	  System.out.println("\n4-Cha");
 	  System.out.println("\n5-Quer adicionar mais Ingredientes?");
-      System.out.println("\n\n-Qual opcao?");
-      BufferedReader Teclado1 = new BufferedReader(new InputStreamReader(System.in));     //Chamando função que lê teclado
+          System.out.println("\n\n-Qual opcao?");
+      BufferedReader Teclado1 = new BufferedReader(new InputStreamReader(System.in));     //Chamando funÃ§Ã£o que lÃª teclado
 
-      int A = Integer.parseInt(Teclado1.readLine());    //Lendo o número que o usuário botou na tela e aplicando no switch case......
+      int A = Integer.parseInt(Teclado1.readLine());    //Lendo o nÃºmero que o usuÃ¡rio botou na tela e aplicando no switch case......
 
        switch(A) {
 
 		   case 1:    //caso 1 acontece
 		   {
 
-			   if(IngredienteQuantidade[0] >= 5 && IngredienteQuantidade[2] >= 2) {     //verificando se temos a quantidade de ingredientes disponíveis
-				     System.out.println("Voce pode comprar CafeComLeite");       //Imprimindo na tela que o usuário pode comprar
-				     System.out.println("Quantidade de CafeEmPo: " + IngredienteQuantidade[0]);    //Imprimindo a quantidade dos ingredientes na tela para o usuário
+			   if(IngredienteQuantidade[0] >= 5 && IngredienteQuantidade[2] >= 2) {     //verificando se temos a quantidade de ingredientes disponÃ­veis
+				     System.out.println("Voce pode comprar CafeComLeite");       //Imprimindo na tela que o usuÃ¡rio pode comprar
+				     System.out.println("Quantidade de CafeEmPo: " + IngredienteQuantidade[0]);    //Imprimindo a quantidade dos ingredientes na tela para o usuÃ¡rio
 				     System.out.println("Quantidade de Leite: " + IngredienteQuantidade[2]);
 				}
-				else {   //Em caso não tenha a quantidade exata mostra isso para o usuário
+				else {   //Em caso nÃ£o tenha a quantidade exata mostra isso para o usuÃ¡rio
 					 System.out.println("Voce nao pode comprar CafeComLeite");
 					 System.out.println("Quantidade de CafeEmPo: " + IngredienteQuantidade[0]);
-				     System.out.println("Quantidade de Leite: " + IngredienteQuantidade[2]);
+				         System.out.println("Quantidade de Leite: " + IngredienteQuantidade[2]);
 				}
 				break;  //Encerra case 1
 			   }
@@ -49,12 +49,12 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 			    if(IngredienteQuantidade[1] >= 10 && IngredienteQuantidade[6] >= 30) {
 					System.out.println("Voce pode comprar CookieRecheado!");
 					System.out.println("Quantidade de Cookie: " + IngredienteQuantidade[1]);
-				    System.out.println("Quantidade de Chocolate: " + IngredienteQuantidade[6]);
+				        System.out.println("Quantidade de Chocolate: " + IngredienteQuantidade[6]);
 				}
 				else {
 					 System.out.println("Voce nao pode comprar CookieRecheado!");
 					 System.out.println("Quantidade de Cookie: " + IngredienteQuantidade[1]);
-				     System.out.println("Quantidade de Chocolate: " + IngredienteQuantidade[6]);
+				         System.out.println("Quantidade de Chocolate: " + IngredienteQuantidade[6]);
 				}
 				break;
 		   }
@@ -65,8 +65,8 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 				     System.out.println("Quantidade de Pao: " + IngredienteQuantidade[3]);
 				     System.out.println("Quantidade de Frios: " + IngredienteQuantidade[5]);
 			   }
-			   else {
-				     System.out.println("Voce nao pode comprar PaocomMortandela!");
+			   else { 
+				      System.out.println("Voce nao pode comprar PaocomMortandela!");
 				      System.out.println("Quantidade de Pao: " + IngredienteQuantidade[3]);
 				      System.out.println("Quantidade de Frios: " + IngredienteQuantidade[5]);
 			   }
@@ -75,8 +75,8 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 		   case 4: {  //Inicia case 4
 
 			   if(IngredienteQuantidade[4] >= 1) {
-                  System.out.println("Voce pode comprar cha!");
-                  System.out.println("Quantidade de Cha: " + IngredienteQuantidade[4]);
+                                System.out.println("Voce pode comprar cha!");
+                                System.out.println("Quantidade de Cha: " + IngredienteQuantidade[4]);
 
 			   }
 			   else {
@@ -87,29 +87,29 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 		   }
 		   case 5: //Case 5 iniciado
 
-		        System.out.println("Ingredientes: ");   //Imprime as opções
-			    System.out.println("\n1-CafeEmPo");
-			    System.out.println("\n2-Cookie");
-			    System.out.println("\n3-Leite");
-			  	System.out.println("\n4-Pao");
-			    System.out.println("\n5-Cha");
-			    System.out.println("\n6-Frios");
-			    System.out.println("\n7-Chocolate");
-	            System.out.println("\n\n-Qual opcao: ");
-			    BufferedReader Teclado2 = new BufferedReader(new InputStreamReader(System.in));  //Chamando função que Lê teclado novamente
+		        System.out.println("Ingredientes: ");   //Imprime as opÃ§Ãµes
+		        System.out.println("\n1-CafeEmPo");
+		        System.out.println("\n2-Cookie");
+			System.out.println("\n3-Leite");
+			System.out.println("\n4-Pao");
+			System.out.println("\n5-Cha");
+			System.out.println("\n6-Frios");
+		        System.out.println("\n7-Chocolate");
+	                System.out.println("\n\n-Qual opcao: ");
+			    BufferedReader Teclado2 = new BufferedReader(new InputStreamReader(System.in));  //Chamando funÃ§Ã£o que LÃª teclado novamente
 
-                int B = Integer.parseInt(Teclado2.readLine());  //Passando o número para a variável B
+                        int B = Integer.parseInt(Teclado2.readLine());  //Passando o nÃºmero para a variÃ¡vel B
 
       switch(B) {  //Criando outro switch case
 
 		   case 1: {  //Iniciando case 1
 
 			    System.out.println("Quanto deste produto voce quer adicionar?: ");
-			    BufferedReader Teclado3 = new BufferedReader(new InputStreamReader(System.in));    //Lê teclado do usuário
-                int C = Integer.parseInt(Teclado3.readLine());   //Passa o dado para a variável C
+			    BufferedReader Teclado3 = new BufferedReader(new InputStreamReader(System.in));    //LÃª teclado do usuÃ¡rio
+                            int C = Integer.parseInt(Teclado3.readLine());   //Passa o dado para a variÃ¡vel C
 
-                IngredienteQuantidade[0] =  IngredienteQuantidade[0] + C;    //Faz a somatória do número da Array antiga com o atual
-                System.out.println("Agora voce tem de CafeEmPo: " + IngredienteQuantidade[0]);  //Imprime na tela o novo valor da array
+                            IngredienteQuantidade[0] =  IngredienteQuantidade[0] + C;    //Faz a somatÃ³ria do nÃºmero da Array antiga com o atual
+                            System.out.println("Agora voce tem de CafeEmPo: " + IngredienteQuantidade[0]);  //Imprime na tela o novo valor da array
 			     break;  //Encerra case 1
 		   }
 
@@ -177,7 +177,7 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 			    IngredienteQuantidade[6] =  IngredienteQuantidade[6] + C;
 			    System.out.println("Agora voce tem de Chocolate: " + IngredienteQuantidade[6]);
 			    break;
-			    default:   //Método padrão caso as outras opções não sejam escolhidas pelo usuário
+			    default:   //MÃ©todo padrÃ£o caso as outras opÃ§Ãµes nÃ£o sejam escolhidas pelo usuÃ¡rio
 			    System.out.println("\nOpcao Invalida!!!");
 
 
@@ -186,7 +186,7 @@ class ProdutosdeUmaLoja   //Criando classe do programa
 
 
 
-			      default:   //Método padrão caso as outras opções não sejam escolhidas pelo usuário
+			      default:   //MÃ©todo padrÃ£o caso as outras opÃ§Ãµes nÃ£o sejam escolhidas pelo usuÃ¡rio
 			      System.out.println("\nOpcao invalida!!!");
 
 
